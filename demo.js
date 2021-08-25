a517ed65af58815af5f5c58a5c551f804bf302f1
Siddharth <siddharth.muscat@gmail.com>
2021-08-25 14:50:01 +0530

    Stuff

---
+++ demo.js
@@ -0,0 +1,28 @@
+const parse = require('./index');
+
+console.log(parse(`
+<script>
+	import Hello from 'hello.fwrk';
+	const name = 'world';
+
+	function getColor(text) {
+		return '#' + text.toLowerCase().slice(0, 6).padEnd(6, 0).replace(/[^0-9a-f]/g, 0);
+	}
+
+	let seconds = 0;
+
+	setInterval(() => seconds++, 1000)
+</script>
+
+<Hello/>
+
+<p style='color: {getColor(name)}'>This is your color</p>
+
+<p>You have been here for {seconds} seconds</p>
+
+<style>
+	* {
+		color: red;
+	}
+</style>
+`.trim()))
---
+++ resolve.js
@@ -0,0 +1,33 @@
+const recast = require('recast');
+const fs = require('fs');
+
+const split = require('./split');
+
+const b = recast.types.builders;
+
+module.exports = function resolve({HTML = '', CSS = '', JS = ''}) {
+	const ast = recast.parse(JS);
+
+	recast.visit(ast, {
+		visitImportDeclaration(path) {
+			const {HTML: _HTML, CSS: _CSS, JS: _JS} = split(fs.readFileSync(path.node.source.value, 'utf8'));
+			const name = path.node.specifiers.local.name;
+
+			console.log(typeof HTML);
+			HTML = HTML.replace(new RegExp(`(<${name}><\/${name}>|<${name}\/?>)`, 'g'), _HTML)
+
+			CSS += _CSS;
+
+			path.replace(
+				b.blockStatement(
+					recast.parse(_JS).program.body
+				)
+			)
+			return false;
+		}
+	});
+
+	JS = recast.print(ast).code;
+
+	return {HTML, CSS, JS}
+} 
