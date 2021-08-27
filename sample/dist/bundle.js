323e3e04da332e9c3184441fec5a2c91a89269fb
Siddharth <siddharth.muscat@gmail.com>
2021-08-27 19:14:25 +0530

    Better samples

--- sample/dist/bundle.js
+++ sample/dist/bundle.js
@@ -1,9 +1,19 @@
 {
   {
-    setInterval(() => document.querySelector('h1').classList.toggle('show'), 1000);
+    {
+      {
+        {
+          {
+            {
+              {
+                {
+                  {}
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
-}
-
-{
-  setInterval(() => document.querySelector('p').classList.toggle('show'), 1000);
 }
