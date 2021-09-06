ad7960c9f268534fb9fba6ee95f2eb5d97c15d3d
Siddharth <siddharth.muscat@gmail.com>
2021-09-06 20:30:12 +0530

    Major work on reactive engine
    GitHub CoPilot wrote the code :D

--- src/react-2.js
+++ src/react-2.js
@@ -1,25 +1,64 @@
-import {parse, print, visit, types} from 'recast';
 import chalk from 'chalk';
+import {parse, print, types, visit} from 'recast';
 
-const {builders: b} = types;
+const {builders: b, namedTypes: n} = types;
 
 export function makeReactive(JS) {
 	const ast = parse(JS);
 
 	visit(ast, {
 		visitExpressionStatement(path) {
-			this.traverse(path);
 
-			path.replace(
-				b.expressionStatement(
+			// TODO: when assignments are called together, update only once
+			// Sample:
+			// let a = 0;
+			//
+			// a++
+			// a += 10;
+			// =>
+			// ...
+			// a++
+			// a += 10;
+			// reactTo('a');
+			if (n.AssignmentExpression.check(path.node.expression)) {
+				path.replace(path.node, b.expressionStatement(
 					b.callExpression(
-						b.identifier('__change'),
-						[]
+						b.identifier('__reactTo'),
+						[b.literal(path.node.expression.left.name)]
 					)
-				)
-			);
+				))
+			}
+
+			if (n.UpdateExpression.check(path.node.expression)) {
+				path.replace(path.node, b.expressionStatement(
+					b.callExpression(
+						b.identifier('__reactTo'),
+						[b.literal(path.node.expression.argument.name)]
+					)
+				))
+			}
+
+			this.traverse(path);
+		},
+
+		visitLabeledStatement(path) {
+			if (path.node.label.name === '$') {
+				path.replace(b.expressionStatement(
+					b.callExpression(
+						b.identifier('__labelled'),
+						[b.functionExpression(
+							null,
+							[], // TODO dependencies
+							b.blockStatement([path.node.body])
+						)]
+					))
+				);
+			}
+
+			this.traverse(path);
 		}
 	});
+
 
 	JS = print(ast).code;
 
@@ -35,6 +74,7 @@ $: console.log(foo * 2);
 // later...
 foo = 10;
 foo++;
+foo += 10;
 `.trim();
 
 const changed = makeReactive(original);
