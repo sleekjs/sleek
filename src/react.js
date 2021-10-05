d3d03d2fd420f438c08be2e6c8d73dc6cef8d866
Siddharth <siddharth.muscat@gmail.com>
2021-10-05 15:18:52 +0530

    Detect variable declarations and update

--- src/react.js
+++ src/react.js
@@ -1,33 +1,51 @@
 import {parse, print, types, visit} from 'recast';
 
-const {builders: b, namedTypes: n} = types;
+const {builders: b} = types;
 
 export function makeReactive(JS) {
 	const ast = parse(JS);
 
-	function visitUpdate(path) {
-		console.log(print(path).code);
-		console.log();
-
-		path.replace(
-			b.blockStatement([
-				b.expressionStatement(path.node),
-				b.expressionStatement(
-					b.callExpression(
-						b.identifier('__update__' + path.node.argument.name),
-						[]
-					)
-				)
-			])
-		);
-		return false;
-	}
-
 	visit(ast, {
-		visitUpdateExpression: visitUpdate
+		visitUpdateExpression: visitUpdate,
+		visitVariableDeclaration: visitVariable
 	});
 
 	JS = print(ast).code;
 
 	return JS;
+}
+
+function visitVariable(path) {
+	// `const` is constant
+	if (path.node.kind === 'const') return false;
+
+	const reactiveDeclarators = [];
+
+	for (const declaration of path.node.declarations) {
+		console.log(declaration);
+		reactiveDeclarators.push(b.functionDeclaration(
+			b.identifier(`__update__${declaration.id.name}`),
+			[],
+			b.blockStatement([])
+		));
+	}
+
+	path.replace(path.node, ...reactiveDeclarators);
+
+	return false;
+}
+
+function visitUpdate(path) {
+	path.replace(
+		b.blockStatement([
+			b.expressionStatement(path.node),
+			b.expressionStatement(
+				b.callExpression(
+					b.identifier('__update__' + path.node.argument.name),
+					[]
+				)
+			)
+		])
+	);
+	return false;
 }
